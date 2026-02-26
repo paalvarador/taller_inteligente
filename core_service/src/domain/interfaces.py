5ad3870479818fda333c216d4a80c86b1393321f
Pablo Antonio Alvarado Ruiz <paalvarador@gmail.com>
2026-02-26 16:41:12 -0500

    feat: Creando archivo interfaces con los Repositories correspondientes

--- core_service/src/domain/interfaces.py
+++ core_service/src/domain/interfaces.py
@@ -1,2 +1,17 @@
 from abc import ABC, abstractmethod
 from .entities import Vehiculo, Medicion
+
+class IVehiculoRepository(ABC):
+    @abstractmethod
+    def save(self, vehiculo: Vehiculo):
+        pass
+    
+    @abstractmethod
+    def get_by_placa(self, placa: str) -> Vehiculo:
+        pass
+
+
+class IMedicionRepository(ABC):
+    @abstractmethod
+    def save(self, medicion: Medicion):
+        pass
